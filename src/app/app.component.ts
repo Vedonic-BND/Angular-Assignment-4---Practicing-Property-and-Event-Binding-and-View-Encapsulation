27c1d514af2947470a3c0fa187266e2a6275bf77
Vedonic-BND <vdn3903@students.uc-bcf.edu.ph>
2023-05-03 12:52:24 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment_3-Practicing_Property_and_Event_Binding_and_View_Encapsulation';
+}
